aea6b722c2b2ace067e5ed1f8ce7cc171e2c78c0
polyapers <55540787+polyapers@users.noreply.github.com>
2023-08-29 00:52:15 +0300

    Update FGExpan.py

--- FGExpan.py
+++ FGExpan.py
@@ -1,1 +1,98 @@
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from transformers import pipeline
+
+
+class CGExpan(object):
+
+  // initialization 
+
+  def 
+
+  #Function to calculate the Type Generation Score
+  def type_generation_score(entity_set, entity_type, hearst_patterns, language_model):
+      total_similarity = 0.0
+      pattern_count = len(hearst_patterns)
+      token_count = 0
+  
+      for pattern in hearst_patterns:
+          # Generate the Type-generating query
+          query = TypeGeneratingQuery(pattern, entity_set[0])  # Assume entity_set is a list
+          query_text = query.generate_query()
+  
+          # Get the predicted tokens for the query
+          predicted_tokens = language_model.predict_tokens(query_text)
+  
+          for token in predicted_tokens.get_top_tokens(5):  # Consider top 5 predicted tokens
+              # Calculate the cosine similarity between embeddings
+              token_embedding = language_model.get_embedding(token)
+              entity_type_embedding = language_model.get_embedding(entity_type)
+              similarity = cosine_similarity([token_embedding], [entity_type_embedding])[0][0]
+  
+              total_similarity += similarity
+              token_count += 1
+  
+      if token_count == 0:
+          return 0.0  # Avoid division by zero
+  
+      return total_similarity / (pattern_count * token_count)
+
+  #Function to calculate the Entity Generation Score
+  def entity_generation_score(entity_set, entity_type, hearst_patterns, language_model):
+      total_similarity = 0.0
+      pattern_count = len(hearst_patterns)
+      token_count = 0
+      entity_count = len(entity_set)
+  
+      for pattern in hearst_patterns:
+          for entity in entity_set:
+              # Generate the Entity-generating query
+              query = EntityGeneratingQuery(pattern, entity)
+              query_text = query.generate_query()
+  
+              # Get the predicted tokens for the query
+              predicted_tokens = language_model.predict_tokens(query_text)
+  
+              for token in predicted_tokens.get_top_tokens(5):  # Consider top 5 predicted tokens
+                  # Calculate the cosine similarity between embeddings
+                  token_embedding = language_model.get_embedding(token)
+                  entity_embedding = language_model.get_embedding(entity)
+  
+                  similarity = cosine_similarity([token_embedding], [entity_embedding])[0][0]
+  
+                  total_similarity += similarity
+                  token_count += 1
+  
+      if token_count == 0:
+          return 0.0  # Avoid division by zero
+  
+      return total_similarity / (pattern_count * token_count * entity_count)
+    
+  
+  # Initialize the NLI model for entailment
+  nli_model = pipeline("textual-entailment")
+  
+  # Function to calculate the Entailment Score
+  def entailment_score(entity_set, entity_type, sentence_samples):
+      total_score = 0.0
+      entity_count = len(entity_set)
+      sample_count = len(sentence_samples)
+  
+      for entity in entity_set:
+          for sentence in sentence_samples[entity]:
+              # Create the entailment hypothesis
+              hypothesis = f"In this sentence, {entity} is a type of {entity_type}."
+  
+              # Calculate the entailment score using the NLI model
+              entailment_result = nli_model(sentence, hypothesis)
+  
+              # Extract the score (assuming 'score' is a key in the result)
+              entailment_score = entailment_result[0]["score"]
+  
+              total_score += entailment_score
+  
+      if entity_count == 0 or sample_count == 0:
+          return 0.0  # Avoid division by zero
+  
+      return total_score / (sample_count * entity_count)
 
